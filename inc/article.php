4bf8854ef38dddf570eecef2cedbf94ec7b2cf0b
s10n <a@akaiv.com>
2014-12-06 14:10:19 +0900

    함수: RSS의 본문 앞에 썸네일과 원문 링크를 추가하는 함수의 오작동 해결 및 개선

--- inc/article.php
+++ inc/article.php
@@ -5,12 +5,16 @@ remove_filter( 'the_excerpt', 'wpautop' );
 /* 피드 */
 function akaiv_insert_footnote($content) {
   if ( is_feed() || is_single() ) :
-    $content = '<p>'.$content.'</p>';
-    if ( is_feed() && has_post_thumbnail() ) :
-      $content = '<p>'.get_the_post_thumbnail(null, 'large').'</p>'.$content;
+    $content   = ( ! empty($content) ) ? '<p>'.$content.'</p>' : '';
+    $thumbnail = ( has_post_thumbnail() ) ? '<p>'.get_the_post_thumbnail(null, 'large').'</p>' : '';
+    $original  = '<p>원문 바로가기: <a href="'.akaiv_get_url().'" target="_blank" rel="bookmark">'.akaiv_get_url().'</a></p>';
+    if ( is_feed() ) :
+      $content = $thumbnail.$original.$content;
+    elseif ( is_single() ) :
+      $content = $content.$original;
     endif;
-    $content.= '<p>원문 바로가기: <a href="'.akaiv_get_url().'" target="_blank" rel="bookmark">'.akaiv_get_url().'</a></p>';
   endif;
   return $content;
 }
-add_filter( 'the_excerpt', 'akaiv_insert_footnote' );
+add_filter( 'the_excerpt',     'akaiv_insert_footnote' );
+add_filter( 'the_excerpt_rss', 'akaiv_insert_footnote' );
